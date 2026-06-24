bf30b58d0272f4b276dc551344d5be464a33e524
Mihir Kandoi <kandoimihir@gmail.com>
2026-06-24 20:37:34 +0530

    refactor: parse native JSON request args in selling/doctype/quotation/mapper.py
    Use frappe.parse_json instead of json.loads so the whitelisted endpoints
    accept native JSON types (list/dict/bool) in addition to JSON strings.

--- erpnext/selling/doctype/quotation/mapper.py
+++ erpnext/selling/doctype/quotation/mapper.py
@@ -31,8 +31,7 @@ def make_sales_order(
 def _make_sales_order(source_name, target_doc=None, ignore_permissions=False, args=None):
 	if args is None:
 		args = {}
-	if isinstance(args, str):
-		args = json.loads(args)
+	args = frappe.parse_json(args)
 
 	customer = _make_customer(source_name, ignore_permissions)
 	ordered_items = get_ordered_items(source_name)
@@ -151,8 +150,7 @@ def make_sales_invoice(
 def _make_sales_invoice(source_name, target_doc=None, ignore_permissions=False, args=None):
 	if args is None:
 		args = {}
-	if isinstance(args, str):
-		args = json.loads(args)
+	args = frappe.parse_json(args)
 
 	customer = _make_customer(source_name, ignore_permissions)
 
